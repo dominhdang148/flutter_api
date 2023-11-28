61b6c4bb0122cb1acffd8c80e80d79617e09b8b1
dominhdang148 <myclone235@gmail.com>
2023-11-29 01:56:29 +0700

    finsihed part 1

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +11,48 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<dynamic> users = [];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        title: const Text("Rest API call"),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final user = users[index];
+          return ListTile(
+            leading: CircleAvatar(
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: Image.network(user['picture']['thumbnail'])),
+            ),
+            title: Text(user["email"]),
+          );
+        },
+        itemCount: users.length,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _fetchUser,
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+
+  void _fetchUser() async {
+    debugPrint("Fetch user called");
+    const url = 'https://randomuser.me/api?results=100';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    setState(() {
+      users = json['results'];
+    });
+
+    debugPrint("Fetch user completed");
   }
 }
